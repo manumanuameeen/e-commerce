503e6763d7670bc75d15dc802917efae7ac7bdec
manumanuameeen <jerrykerry3806@gmail.com>
2025-03-05 11:08:07 +0530

    google auth

--- src/config/passport.js
+++ src/config/passport.js
@@ -5,7 +5,7 @@ import User from "../models/userSchema.js";
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'http://localhost:3000/auth/google/callback'
+    callbackURL: "https://aranoz.zapto.org/auth/google/callback" 
 },
 async (accessToken, refreshToken, profile, done) => {
     console.log("now passport configuration going on");
