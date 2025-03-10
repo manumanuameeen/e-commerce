05dfa54e647ac54d8e8af1f5a1074ad520d7cce6
manumanuameeen <jerrykerry3806@gmail.com>
2025-03-10 16:16:58 +0530

    file created

--- src/controllers/admin/productControlller.js
+++ src/controllers/admin/productControlller.js
@@ -202,7 +202,7 @@ const getEditProduct = async (req, res) => {
         const categories = await Category.find();
         const currentCategory = await Category.findById(product.category);
         
-        res.render('edit-Product', { 
+        res.render('admin_product_edit', { 
             product: product,
             cat: categories,
             currentCategoryName: currentCategory ? currentCategory.name : ''
